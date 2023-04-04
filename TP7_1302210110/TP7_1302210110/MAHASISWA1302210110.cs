85f5be02e596aac19b4cff7b0ea26a4f7c078a5b
Praktikan <fasyaraihan82@gmail.com>
2023-04-04 16:38:10 +0700

    menambahkan class mahasiswa1302210110

---
+++ TP7_1302210110/TP7_1302210110/MAHASISWA1302210110.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace jurnal7_1302210110
+{
+    public class DataMahasiswa1302210110
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String gender { get; set; }
+        public int age { get; set; }
+        public address adr { get; set; }
+        public course matkul { get; set; }
+
+        public DataMahasiswa1302210110(String firstName, String lastName, String gender, int age, address address, course matkul)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.age = age;
+            this.adr = address;
+            this.matkul = matkul;
+
+        }
+
+        public static void ReadJSON()
+        {
+            String jsonString = File.ReadAllText("D:\\modul7_kelompok_2");
+
+            DataMahasiswa1302210110 mahasiswa = JsonSerializer.Deserialize<DataMahasiswa1302210110>(jsonString);
+
+            Console.WriteLine("mahasiswa dengan nama " + mahasiswa.firstName + mahasiswa.lastName + " kelamin " + mahasiswa.gender + " umur " + mahasiswa.age + " alamat " + mahasiswa.matkul);
+
+        }
+
+        public class address
+        {
+            public String streetAddress;
+            public String city;
+            public String state;
+
+            public address(String adr, String city, String state)
+            {
+                this.streetAddress = adr;
+                this.city = city;
+                this.state = state;
+            }
+        }
+        public class course
+        {
+            public String code;
+            public String name;
+
+            public course(String code, String name)
+            {
+                this.code = code;
+                this.name = name;
+            }
+        }
+    }
+
+
+}
