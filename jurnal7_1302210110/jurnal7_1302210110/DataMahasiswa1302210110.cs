2275e27c06c28b40e3ff80fa2bd00aec461cbe78
Praktikan <fasyaraihan82@gmail.com>
2023-04-04 16:22:27 +0700

    menambahkan class mahasiswa1302210110

---
+++ jurnal7_1302210110/jurnal7_1302210110/DataMahasiswa1302210110.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal7_1302210110
+{
+    internal class DataMahasiswa1302210110
+    {
+    }
+}
---
+++ jurnal7_1302210110/jurnal7_1302210110/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal7_1302210110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
