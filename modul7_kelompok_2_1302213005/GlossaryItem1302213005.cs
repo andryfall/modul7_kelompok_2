c745215c4ebd6271ebf37d4eeb8753bcc30e1b04
andryfall <>
2023-04-04 17:13:55 +0700

    menambahkan GlossaryItems1302213005 (WIP)

---
+++ modul7_kelompok_2_1302213005/GlossaryItem1302213005.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_2_1302213005
+{
+    internal class Class1
+    {
+    }
+}
